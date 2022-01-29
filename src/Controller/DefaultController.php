d24ffefdfbd3eb3456266e01b902dfbd9d17abf6
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2022-01-29 14:05:11 +0300

    edit profile

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,13 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="main_homepage")
      */
     public function index(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         $productList = $entityManager->getRepository(Product::class)->findAll();
-        dd($productList);
+        //dd($productList);
         return $this->render('main/default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
